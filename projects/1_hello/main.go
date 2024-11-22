b8c239b1441a39555dd38d5b454a77193c216b95
bigd <bogdan.istamilov@bk.ru>
2024-11-22 20:29:42 +0300

    hello

--- projects/1_hello/main.go
+++ projects/1_hello/main.go
@@ -1,7 +1,14 @@
 package main
 
-// здесь надо написать код
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	// и здесь тоже
+	http.HandleFunc("/get", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, web!")
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
